e10e2ea8d53414e52d3d67a246a3e1b83ade549b
DVZhuk <107407634+DVZhuk@users.noreply.github.com>
2022-06-22 18:21:48 +0300

    Add files via upload
    Светлая тема

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-let upButton = document.querySelector('.up-button')
+let upButton = document.querySelector('.up-button');
+let theme = document.querySelector('.theme');
+let themeButton = document.querySelector('.themes');
+let themeText = document.querySelector('.theme-text')
+
+themeButton.onclick = function () {
+  theme.classList.toggle('light-theme');
+  if (theme.classList.contains('light-theme')) {
+    themeText.textContent = 'Тёмная тема';
+  } else {
+    themeText.textContent = 'Светлая тема';
+  };
+};
 
 window.onscroll = function () {
   if (window.pageYOffset > 2000) {
