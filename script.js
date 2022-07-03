3c6b2fd6d887b39cf6b54b753920fab884d23b58
DVZhuk <107407634+DVZhuk@users.noreply.github.com>
2022-07-03 11:22:35 +0300

    Обновление сетки

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ let upButton = document.querySelector('.up-button');
 let theme = document.querySelector('.theme');
 let themeButton = document.querySelector('.themes');
 let themeText = document.querySelector('.theme-text');
+let nans = document.querySelectorAll('.nan')
 // Ключ памяти темы
 let key = localStorage.getItem('key');
 if (key == 'theme light-theme') {
   theme.classList.add('light-theme');
+  themeText.textContent = 'Тёмная тема';
+  for (let nan of nans) {
+  nan.src = 'source/crap_1.png' };
 };
 
 
@@ -13,8 +17,12 @@ themeButton.onclick = function () {
   theme.classList.toggle('light-theme');
   if (theme.classList.contains('light-theme')) {
     themeText.textContent = 'Тёмная тема';
+    for (let nan of nans) {
+    nan.src = 'source/crap_1.png' };
   } else {
     themeText.textContent = 'Светлая тема';
+    for (let nan of nans) {
+    nan.src = 'source/crap_2.png' };
   };
   // Сохранение в память состаяния класса темы
   localStorage.setItem('key', theme.classList);
